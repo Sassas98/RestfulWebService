149cc095114d3f8cbde6b9b82e5d5f323f995d0f
Marvin <marvin.sincini@studenti.unicam.it>
2023-12-13 08:36:23 +0100

    [piccoli aggiustamenti]

--- src/main/java/marvin/work/controller/ProductServiceController.java
+++ src/main/java/marvin/work/controller/ProductServiceController.java
@@ -5,28 +5,17 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import marvin.work.ProductListRepository;
 import marvin.work.exception.ProductNotFoundException;
 import marvin.work.model.Product;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.websocket.server.PathParam;
@@ -48,7 +37,6 @@ public class ProductServiceController {
 	}*/
 
 	private ProductListRepository productRepository;
-	@Autowired
 	public ProductServiceController(ProductListRepository productRepository) {
 		this.productRepository=productRepository;
 		Product miele = new Product();
@@ -92,7 +80,7 @@ public class ProductServiceController {
 		}
 	}
 	
-	@RequestMapping(value="/products", method = RequestMethod.PUT)
+	@PutMapping("/products")
 	public ResponseEntity<Object> updateProduct(@PathParam("id") String id,@RequestBody Product product){
 		if(productRepository.existsById(id)) {
 			product.setId(id);
@@ -104,7 +92,7 @@ public class ProductServiceController {
 	//private static String path = "/Users/15mim/OneDrive/Desktop/";
 	private static String path = "/Users/Marvin/Desktop/";
 	
-	@RequestMapping(value="/fileUpload", method = RequestMethod.POST, 
+	@PostMapping(value="/fileUpload", 
 			consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<Object> fileUpload(@RequestParam("file") MultipartFile file,
 			@RequestParam("filename") String filename) throws IOException {
